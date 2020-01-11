970dc870d04899590e61bf828f79fc9a9b819ccd
Max <mmcevoy@ualberta.ca>
2020-01-11 07:40:19 -0700

    Working on adding in tasks

--- Reader.py
+++ Reader.py
@@ -1,4 +1,6 @@
 import csv
+import os
+import shutil
 
 class Staff:
     def __init__(self, file):
@@ -31,11 +33,50 @@ class Staff:
 
     def update_sheet(self):
         #Updates the csv file and reorders it based on point values
-        with open("staff.csv", "w", newline='') as csvfile:
+        with open(self.file, "w", newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
             writer.writeheader()
             for key, value in sorted(self.staff.items(), key=lambda e: int(e[1][2]), reverse=True):
                 writer.writerow({'name': key, "position": self.staff[key][0], "team": self.staff[key][1], "points": self.staff[key][2]})
 
+class Duties:
+    def __init__(self, file):
+        self.file = file
+        self.tasks= {}
+        self.fieldnames=['task','frequency','points']
+        with open(self.file, newline='') as csvfile:
+            reader = csv.DictReader(csvfile, fieldnames=self.fieldnames)
+            next(csvfile)
+            for row in reader:
+                self.tasks[row["task"]] = [row["frequency"], row["points"]]
+
+
+    def point_value(self, task):
+        return self.tasks[task][1]
+'''
+    def subtract_points(self, name, points):
+        self.staff[name][2]= str(int(self.staff[name][2]) - points)
+        self.update_sheet()
+
+    def add_staff(self, name, position, team, points):
+        self.staff[name] = [position, team, points]
+        self.update_sheet()
+
+    def remove_staff(self, name):
+        if name in self.staff:
+            del self.staff[name]
+            self.update_sheet()
+
+    def update_sheet(self):
+        #Updates the csv file and reorders it based on point values
+        with open(self.file, "w", newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
+            writer.writeheader()
+            for key, value in sorted(self.staff.items(), key=lambda e: int(e[1][2]), reverse=True):
+                writer.writerow({'name': key, "position": self.staff[key][0], "team": self.staff[key][1], "points": self.staff[key][2]})
+'''
+
+
 if __name__ == '__main__':
     m = Staff('staff.csv')
+    n = Duties('maintenance.csv')
--- main.py
+++ main.py
@@ -1,17 +1,97 @@
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
 from kivy.app import runTouchApp
 from Reader import Staff
+from Reader import Duties
+from kivy.uix.tabbedpanel import TabbedPanel
+import os
 
-def names(info):
+
+def title_screen():
+    def go_to_daily(instance):
+        titlecard.clear_widgets(children=None)
+        duties(Duties("maintenance.csv"))
+
+    intro = AnchorLayout(anchor_x='center', anchor_y='top',size_hint=(1,0.3))
+    title = Label(text="Welcome Please pick an option",
+                  halign="center",
+                  valign="center",
+                  size_hint=(1,1))
+    intro.add_widget(title)
+
+    taskbuttons = BoxLayout(padding = [50,50,50,50], spacing = 20)
+    daily = Button(text="Daily Maintenance",
+                halign="center",
+                valign="center",
+                size_hint=(0.3,1))
+    daily.bind(on_press=go_to_daily)
+
+    weekly = Button(text="Weekly Maintenance",
+                halign="center",
+                valign="center",
+                size_hint=(0.3,1))
+    assigned = Button(text="Assigned Checklists",
+                halign="center",
+                valign="center",
+                size_hint=(0.3,1))
+    lockers = Button(text="Locker Entries",
+                halign="center",
+                valign="center",
+                size_hint=(0.3,1))
+    taskbuttons.add_widget(daily)
+    taskbuttons.add_widget(weekly)
+    taskbuttons.add_widget(assigned)
+    taskbuttons.add_widget(lockers)
+
+    titlecard = BoxLayout(orientation='vertical')
+
+    titlecard.add_widget(intro)
+    titlecard.add_widget(taskbuttons)
+    runTouchApp(titlecard)
+
+
+def duties(info):
+
+    def go_to_names(instance):
+        #This tells where the screen to go next
+        layout.clear_widgets(children=None)
+        names(Staff('staff.csv'), instance.text.split("\n")[2])
+        pass
+
+    layout = GridLayout(cols=4, spacing=20,padding = [10,10,10,10], size_hint_y=None)
+    # Make sure the height is such that there is something to scroll.
+    layout.bind(minimum_height=layout.setter('height'))
+    for key in info.tasks:
+        color = [0.26, 0.71, 0.42,1]
+        btn = Button(text=key+"\n\n"+info.tasks[key][1]+"\npoints",
+                     text_size=(layout.width, None),
+                     size_hint_y=None,
+                     halign="center",
+                     height=400,
+                     background_color=color)
+        btn.bind(on_press=go_to_names)
+        layout.add_widget(btn)
+
+
+    root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+    root.add_widget(layout)
+    runTouchApp(root)
+
+
+
+def names(info, points):
 
     def callback1(instance):
         #This tells where the screen to go next
         name = instance.text
         layout.clear_widgets(children=None)
-        foobar(info, name.split("\n")[0])
+        info.add_points(name.split("\n")[0], int(points))
+        title_screen()
 
     layout = GridLayout(cols=2, spacing=20,padding = [10,10,10,10], size_hint_y=None)
     # Make sure the height is such that there is something to scroll.
@@ -21,6 +101,7 @@ def names(info):
     blue = [0.34, 0.47, 0.91, 1]
     red = [1.74,0,0.01, 1]
     yellow = [2.36, 1.85, 0.57, 1]
+
     for key, value in sorted(info.staff.items(), key=lambda e: int(e[1][2]), reverse=True):
         color = [1, 1, 1, 1]
         if info.staff[key][1]=="Slytherin": color = green
@@ -31,10 +112,17 @@ def names(info):
         btn = Button(text=key + "\n" + info.staff[key][0]+"\n" + info.staff[key][1] + "\n" + info.staff[key][2],
                      size_hint_y=None,
                      halign="center",
-                     height=80,
+                     height=400,
                      background_color=color)
         btn.bind(on_press=callback1)
         layout.add_widget(btn)
+    add_staff = Button(text="Add a Staff Member",
+                       size_hint_y=None,
+                       halign="center",
+                       height=400,
+                       background_color=[1, 1, 1, 1])
+    add_staff.bind(on_press=callback1)
+    layout.add_widget(add_staff)
     root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
     root.add_widget(layout)
     runTouchApp(root)
@@ -43,13 +131,12 @@ def foobar(info, name):
 
     def callback1(instance):
         layout.clear_widgets(children=None)
-        names(info)
+        duties(Duties('maintenance.csv'))
 
-    layout = GridLayout(cols=1,
-                        spacing=20,padding = [10,10,10,10],
-                        size_hint_y=None)
+    layout = AnchorLayout(padding = [50,50,50,50], anchor_x='center', anchor_y='center')
     btn = Button(text="Hello " + name + "\nYou have " + info.staff[name][2] + " points",
-                 halign="center",)
+                 halign="center")
+
     btn.bind(on_press=callback1)
     layout.add_widget(btn)
 
@@ -59,5 +146,10 @@ def foobar(info, name):
 
 
 if __name__ == '__main__':
+    #/storage/emulated/0/kivy/
+    #in final version store saved data there and reference it to that location
     m = Staff('staff.csv')
-    names(m)
+    #names(m)
+    n = Duties('maintenance.csv')
+    #duties(n)
+    title_screen()
